1cfd6086e53b59f9db149e55581a8bcc8b05fcd0
TakaoHoshino <tk.1987.6.9@gmail.com>
2015-03-25 23:14:31 +0800

    Initial Commit

---
+++ OnsenMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnsenMap
+//
+//  Created by 星野嵩夫 on 2015/03/25.
+//  Copyright (c) 2015年 星野嵩夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnsenMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnsenMap
+//
+//  Created by 星野嵩夫 on 2015/03/25.
+//  Copyright (c) 2015年 星野嵩夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
